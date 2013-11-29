2d951f7608c9304fad48b9cabc8d7b9495ada908
wangZf <wzhifang@qq.com>
2013-11-29 13:41:31 +0800

    初始化

---
+++ cocos2d-library/cocos2d_library.h
@@ -0,0 +1,13 @@
+//
+//  cocos2d_library.h
+//  cocos2d-library
+//
+//  Created by wangZf on 13-11-21.
+//  Copyright (c) 2013年 wangZf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cocos2d_library : NSObject
+
+@end
---
+++ cocos2d-library/cocos2d_library.m
@@ -0,0 +1,13 @@
+//
+//  cocos2d_library.m
+//  cocos2d-library
+//
+//  Created by wangZf on 13-11-21.
+//  Copyright (c) 2013年 wangZf. All rights reserved.
+//
+
+#import "cocos2d_library.h"
+
+@implementation cocos2d_library
+
+@end
